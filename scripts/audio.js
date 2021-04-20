7df0944167f5ee3015d098e69608f33585d1812b
Yuriko Anzai Schumacher <yuriko.schumacher@gmail.com>
2021-04-19 23:05:52 -0400

    update swiper / audio

---
+++ scripts/audio.js
@@ -0,0 +1,158 @@
+// ---------- AUDIO PLAYER FOR QUOTES ----------
+// audio autoplay on
+const audioMute = document.querySelector("#audio__mute");
+const audioMuteText = document.querySelector("#audio__mute__text");
+
+const quoteCam = document.querySelector("#quote__cam");
+const audioCam = quoteCam.querySelector("#audio__cam");
+const audioCamMute = quoteCam.querySelector("#audio__cam__mute");
+const audioCamText = [
+	{ el: quoteCam.querySelector("#audio__cam__1"), delay: 1250 },
+	{
+		el: quoteCam.querySelector("#audio__cam__2"),
+		delay: 3500,
+	},
+	{
+		el: quoteCam.querySelector("#audio__cam__3"),
+		delay: 10000,
+	},
+	{
+		el: quoteCam.querySelector("#audio__cam__4"),
+		delay: 15200,
+	},
+	{
+		el: quoteCam.querySelector("#audio__cam__5"),
+		delay: 20000,
+	},
+	{
+		el: quoteCam.querySelector("#audio__cam__6"),
+		delay: 22500,
+	},
+];
+
+const quoteEmma = document.querySelector("#quote__emma");
+const audioEmma = quoteEmma.querySelector("#audio__emma");
+const audioEmmaMute = quoteEmma.querySelector("#audio__emma__mute");
+const audioEmmaText = [
+	{ el: quoteEmma.querySelector("#audio__emma__1"), delay: 1250 },
+	{
+		el: quoteEmma.querySelector("#audio__emma__2"),
+		delay: 4000,
+	},
+	{
+		el: quoteEmma.querySelector("#audio__emma__3"),
+		delay: 5800,
+	},
+	{
+		el: quoteEmma.querySelector("#audio__emma__4"),
+		delay: 7490,
+	},
+];
+
+const quoteThao = document.querySelector("#quote__thao");
+const audioThao = quoteThao.querySelector("#audio__thao");
+const audioThaoMute = quoteThao.querySelector("#audio__thao__mute");
+const audioThaoText = [
+	{ el: quoteThao.querySelector("#audio__thao__1"), delay: 1250 },
+	{
+		el: quoteThao.querySelector("#audio__thao__2"),
+		delay: 4500,
+	},
+	{
+		el: quoteThao.querySelector("#audio__thao__3"),
+		delay: 7300,
+	},
+	{
+		el: quoteThao.querySelector("#audio__thao__4"),
+		delay: 8700,
+	},
+	{
+		el: quoteThao.querySelector("#audio__thao__5"),
+		delay: 11210,
+	},
+];
+
+// click to activate audio autoplay
+let isMuted = false;
+audioMute.onclick = () => {
+	if (!isMuted) {
+		audioMute.src = "audio/speaker-button.png";
+		audioCamMute.src = "audio/speaker-button.png";
+		audioEmmaMute.src = "audio/speaker-button.png";
+		audioThaoMute.src = "audio/speaker-button.png";
+		audioMuteText.innerText = "Click to deactivate audio autoplay";
+		isMuted = true;
+	} else {
+		audioMute.src = "audio/mute-button.svg";
+		audioCamMute.src = "audio/mute-button.svg";
+		audioEmmaMute.src = "audio/mute-button.svg";
+		audioThaoMute.src = "audio/mute-button.svg";
+		audioMuteText.innerText = "Click to activate audio autoplay";
+		isMuted = false;
+	}
+};
+
+toggleAudioAndQuote("cam", "1", audioCam, audioCamText, audioCamMute);
+toggleAudioAndQuote("emma", "2", audioEmma, audioEmmaText, audioEmmaMute);
+toggleAudioAndQuote("thao", "3", audioThao, audioThaoText, audioThaoMute);
+
+function toggleAudioAndQuote(name, partNum, audioEl, textEl, muteEl) {
+	enterView({
+		selector: `#quote__${name}`,
+		enter: function () {
+			let thisPart = document.querySelector(`#part-${partNum}`);
+			let thisPartClassList = Array.from(thisPart.classList);
+			let isThisPartActive = thisPartClassList.includes(
+				"swiper-slide-active"
+			);
+
+			if (isThisPartActive) {
+				audioEl.muted = false;
+				audioEl.play();
+				textEl.forEach((el) => {
+					setTimeout(function () {
+						el.el.classList.add("quote__text__active");
+					}, el.delay);
+				});
+			}
+		},
+		exit: function () {
+			audioEl.muted = true;
+			textEl.forEach((el) => {
+				el.el.classList.remove("quote__text__active");
+			});
+		},
+		offset: 0.4,
+	});
+
+	muteEl.onclick = () => {
+		if (!isMuted) {
+			audioEl.muted = true;
+			muteEl.src = "audio/mute-button.svg";
+			isMuted = true;
+		} else {
+			audioEl.muted = false;
+			muteEl.src = "audio/speaker-button.png";
+			isMuted = false;
+		}
+	};
+
+	enterView({
+		selector: `#audio__${name}__mute`,
+		enter: function () {
+			let thisPart = document.querySelector(`#part-${partNum}`);
+			let thisPartClassList = Array.from(thisPart.classList);
+			let isThisPartActive = thisPartClassList.includes(
+				"swiper-slide-active"
+			);
+			if (isThisPartActive) {
+				audioEl.muted = true;
+				muteEl.src = "audio/mute-button.svg";
+				textEl.forEach((el) => {
+					el.el.classList.add("quote__text__active");
+				});
+			}
+		},
+		offset: 0.8,
+	});
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,58 +1,93 @@
-// ---------- SWIPER ----------
-const swiperV = new Swiper(".swiper-container-v", {
-	direction: "vertical",
-	spaceBetween: 50,
-	navigation: {
-		nextEl: ".swiper-next-v",
-		prevEl: ".swiper-prev-v",
+// ---------- TITLE SWIPER AUTOPLAY ----------
+const swiperAutoplay = new Swiper("div.swiper-container-autoplay", {
+	spaceBetween: 20,
+	centeredSlides: true,
+	loop: true,
+	autoplay: {
+		delay: 5000,
+		disableOnInteraction: true,
 	},
+	autoplayStop: d3.select("p#stop-auto-swipe").on("click", function () {
+		swiperAutoplay.autoplay.stop();
+		d3.select(this).style("opacity", 0).style("cursor", "default");
+	}),
+});
+
+// ---------- TITLE ANIMATION ----------
+let i = 1;
+while (i < 3) {
+	d3.select(`.title-text__${i}`)
+		.transition()
+		.duration(500)
+		.delay(i * 1000 + 250)
+		.style("opacity", 1);
+	i++;
+}
+
+enterView({
+	selector: ".title-text__3",
+	enter: function (el) {
+		el.classList.add("active");
+	},
+	offset: 0.3,
+});
+
+const swiperIntro = new Swiper(".swiper-container-v", {
+	direction: "vertical",
+	slidesPerView: "auto",
 	pagination: {
 		el: ".swiper-pagination-v",
 		clickable: true,
 	},
 });
 
-const swiperH = new Swiper(".swiper-container-h", {
-	spaceBetween: 50,
-	pagination: {
-		el: ".swiper-pagination-h",
-		clickable: true,
+const swiperPartSwitch = new Swiper(".swiper-container-partSwitch", {
+	navigation: {
+		nextEl: ".swiper-next-h",
+		prevEl: ".swiper-prev-h",
 	},
 });
 
-// ---------- STYLE VERTICAL SWIPER NAV ---------- //
-const verticalSwiperNavs = [
-	d3.select(".swiper-prev-v"),
-	d3.select(".swiper-next-v"),
-];
-
-verticalSwiperNavs[1].style("top", `${window.innerHeight - 100}px`);
-verticalSwiperNavs.forEach((div) => styleSwiperNav(div));
-
-function styleSwiperNav(div) {
-	div.on("mouseover", function () {
-		let thisDiv = d3.select(this);
-		if (thisDiv.classed("swiper-button-disabled") == false) {
-			thisDiv.style("opacity", 0.5).style("cursor", "pointer");
-		}
-	}).on("mouseout", function () {
-		d3.select(this).style("opacity", 0).style("cursor", "default");
-	});
-}
-
-// ---------- REMOVE VERTICAL ELEMENTS WHEN SWIPING HORIZONTALLY ----------
-const paginationV = d3.select(".swiper-pagination-v");
-const horizontalSwiperSlides = [
-	d3.select("#part-1"),
-	d3.select("#part-2"),
-	d3.select("#part-3"),
-];
-
-d3.selectAll(".swiper-container").on("click", function () {
-	if (d3.select("#part-1").classed("swiper-slide-active") == true) {
-		console.log(paginationV);
-		paginationV.style("display", "none");
-	} else {
-		paginationV.style("display", "block");
-	}
+// ---------- SMOOTH SCROLLING ----------
+enterView({
+	selector: "#part-1",
+	enter: function (el) {
+		window.location.href = "#part-1";
+	},
+	offset: 0.6,
 });
+
+// // ---------- STYLE HORIZONTAL SWIPER NAV ---------- //
+// const horizontalSwiperNavs = [
+// 	d3.select(".swiper-prev-h"),
+// 	d3.select(".swiper-next-h"),
+// ];
+
+// horizontalSwiperNavs.forEach((div) => styleSwiperNav(div));
+
+// function styleSwiperNav(div) {
+// 	div.on("mouseover", function () {
+// 		let thisDiv = d3.select(this);
+// 		if (thisDiv.classed("swiper-button-disabled") == false) {
+// 			thisDiv.style("opacity", 1).style("cursor", "pointer");
+// 		}
+// 	}).on("mouseout", function () {
+// 		d3.select(this).style("opacity", 0.5).style("cursor", "default");
+// 	});
+// }
+
+// // ---------- REMOVE VERTICAL ELEMENTS WHEN SWIPING HORIZONTALLY ----------
+// const paginationV = d3.select(".swiper-pagination-v");
+// const horizontalSwiperSlides = [
+// 	d3.select("#part-1"),
+// 	d3.select("#part-2"),
+// 	d3.select("#part-3"),
+// ];
+
+// d3.selectAll(".swiper-container").on("click", function () {
+// 	if (d3.select("#part-1").classed("swiper-slide-active") == true) {
+// 		paginationV.style("display", "none");
+// 	} else {
+// 		paginationV.style("display", "block");
+// 	}
+// });
